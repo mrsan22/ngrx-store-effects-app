2b6e2c4585f94613335479252df4370b9e9fb400
Sanjiv Kumar <sanjiv@ccs.neu.edu>
2018-02-22 22:42:20 -0500

    Added functionality for adding a new pizza

--- src/products/store/effects/pizzas.effects.ts
+++ src/products/store/effects/pizzas.effects.ts
@@ -4,32 +4,63 @@
 // Inject Services here for http calls, etc.
 import { Injectable } from '@angular/core';
 
-import {Effect, Actions} from '@ngrx/effects';
-import { of } from "rxjs/observable/of";
-import { switchMap, map, catchError } from "rxjs/operators";
+import { Effect, Actions } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { switchMap, map, catchError } from 'rxjs/operators';
 
-import * as pizzaAction from "../actions";
+import * as pizzaAction from '../actions';
 import { PizzasService } from '../../services';
 
 @Injectable()
 export class PizzasEffects {
-    constructor(
-        private action$: Actions,
-        private pizzaService: PizzasService
-    ) {}
+  constructor(
+    private action$: Actions,
+    private pizzaService: PizzasService
+  ) {}
 
-    @Effect() // This is an effect, which will dispatch an action back to our reducer
-    // @Effect(dispatch: false) - If we do not want to dispatch an action from an Effect
-    // listen to action here coming from components
-    // loadPizza$ is an Obervable<Action>
-    loadPizzas$ = this.action$.ofType(pizzaAction.LOAD_PIZZAS)
+  @Effect() // This is an effect, which will dispatch an action back to our reducer
+  // @Effect(dispatch: false) - If we do not want to dispatch an action from an Effect
+  // listen to action here coming from components
+  // loadPizza$ is an Obervable<Action>
+  loadPizzas$ = this.action$
+    .ofType(pizzaAction.LOAD_PIZZAS)
     .pipe(
-        // switch to a brand new observable
-        switchMap(() => {
-            return this.pizzaService.getPizzas().pipe(
-                map(pizzas => new pizzaAction.LoadPizzasSuccess(pizzas)),
-                catchError(error => of(new pizzaAction.LoadPizzasFail(error)))
+      // switch to a brand new observable
+      switchMap(() => {
+        return this.pizzaService
+          .getPizzas()
+          .pipe(
+            map(
+              pizzas =>
+                new pizzaAction.LoadPizzasSuccess(pizzas)
+            ),
+            catchError(error =>
+              of(new pizzaAction.LoadPizzasFail(error))
             )
-        })
-    )
+          );
+      })
+    );
+
+  @Effect() // An effect listening to create_pizza action.
+  // Create pizza and then issues a success action back to component.
+  createPizza$ = this.action$
+    .ofType(pizzaAction.CREATE_PIZZA)
+    .pipe(
+      map(
+        (action: pizzaAction.CreatePizza) => action.payload
+      ),
+      switchMap(pizza => {
+        return this.pizzaService
+          .createPizza(pizza)
+          .pipe(
+            map(
+              pizza =>
+                new pizzaAction.CreatePizzaSuccess(pizza)
+            ),
+            catchError(error =>
+              of(new pizzaAction.CreatePizzaFail(error))
+            )
+          );
+      })
+    );
 }
--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -2,62 +2,84 @@ import { Pizza } from 'src/products/models/pizza.model';
 import * as fromPizzas from '../actions/pizzas.actions';
 
 export interface PizzaState {
-    /** 
-     * basic entities structure.
-     * {
-     *   1: {
-     *      id: 1,
-     *      name: ''pizzaname',
-     *      toppping: []
-     *      }
-     * }
-    */
-    entities: {[id: number]: Pizza};
-    loaded: boolean;
-    loading: boolean;
+  /**
+   * basic entities structure.
+   * {
+   *   1: {
+   *      id: 1,
+   *      name: ''pizzaname',
+   *      toppping: []
+   *      }
+   * }
+   */
+  entities: { [id: number]: Pizza };
+  loaded: boolean;
+  loading: boolean;
 }
 export const initialState: PizzaState = {
-    entities: {},
-    loading: false,
-    loaded: false
+  entities: {},
+  loading: false,
+  loaded: false
 };
 
-export function reducer (state = initialState, action: fromPizzas.PizzasAction): PizzaState {
-    switch (action.type) {
-        case fromPizzas.LOAD_PIZZAS: {
-            return {
-                ...state, loading: true
-            };
-        }
-        case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-            // [{id:1, name: string, toppings: []}, {id:2, name: string, toppings: []}]
-            const pizzas = action.payload;
-            // check the reduce function syntax(callbackFn, initialValue)
-            const entities = pizzas.reduce((entities: {[id: number]: Pizza}, pizza: Pizza) => {
-               return {
-                ...entities, [pizza.id]: pizza
-               }; 
-            }, {    
-                // initial value: all the entities
-                ...state.entities
-            });
-
-            return {
-                ...state, loading: false, loaded: true, entities
-            };
-        }
-        case fromPizzas.LOAD_PIZZAS_FAIL: {
-            return {
-                ...state, loading: false, loaded: false
-            };
-        }                                
+export function reducer(state = initialState, action: fromPizzas.PizzasAction): PizzaState {
+  switch (action.type) {
+    case fromPizzas.LOAD_PIZZAS: {
+      return {
+        ...state,
+        loading: true
+      };
     }
-    return state;
+    case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      // [{id:1, name: string, toppings: []}, {id:2, name: string, toppings: []}]
+      const pizzas = action.payload;
+      // check the reduce function syntax(callbackFn, initialValue)
+      const entities = pizzas.reduce(
+        (entities: { [id: number]: Pizza }, pizza: Pizza) => {
+          return {
+            ...entities,
+            [pizza.id]: pizza
+          };
+        },
+        {
+          // initial value: all the entities
+          ...state.entities
+        }
+      );
+
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        entities
+      };
+    }
+    case fromPizzas.LOAD_PIZZAS_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: false
+      };
+    }
+
+    case fromPizzas.CREATE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const entities = {
+        ...state.entities,
+        [pizza.id]: pizza
+      };
+      return {
+        ...state,
+        entities
+      };
+    }
+  }
+  return state;
 }
 
 /**
  * Exposing some levels of pizza state.
- * @param state 
+ * @param state
  */
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
